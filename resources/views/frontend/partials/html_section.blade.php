5472feab5395057e1729037c934f5ea20547eb46
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-03-06 21:07:03 +0700

    update frondend

--- resources/views/frontend/partials/html_section.blade.php
+++ resources/views/frontend/partials/html_section.blade.php
@@ -7,17 +7,41 @@
             <div class="section-content">
                 <div class="soju__prose service-page-long-content">
                     <div class="is-truncated page-content relative">
-                        {!! trans('home.section_html_desc') !!}
+                        <!-- {!! trans('home.section_html_desc') !!} -->
+                        <div class="why-select">
+                            <div class="icon"><i class="fa-regular fa-user"></i></div>
+
+                            <p class="ff-popins fw-700 fs-18">Professional Teacher</p>
+                            <p class="ff-popins fs-14 text-gray">Learn from experienced instructors dedicated to your success.Receive personalized guidance and feedback.</p>
+                        </div>
+                        <div class="why-select">
+                        <div class="icon"><i class="fa-regular fa-user"></i></div>
+
+                        <p class="ff-popins fw-700 fs-18">Professional Teacher</p>
+                        <p class="ff-popins fs-14 text-gray">Learn from experienced instructors dedicated to your success.Receive personalized guidance and feedback.</p>
                     </div>
-                    <div class="cloned-content"></div>
-                    <div class="button-show">
+                    <div class="why-select">
+                        <div class="icon"><i class="fa-regular fa-user"></i></div>
+
+                        <p class="ff-popins fw-700 fs-18">Professional Teacher</p>
+                        <p class="ff-popins fs-14 text-gray">Learn from experienced instructors dedicated to your success.Receive personalized guidance and feedback.</p>
+                    </div>
+                    <div class="why-select">
+                        <div class="icon"><i class="fa-regular fa-user"></i></div>
+                     
+                        <p class="ff-popins fw-700 fs-18">Professional Teacher</p>
+                        <p class="ff-popins fs-14 text-gray">Learn from experienced instructors dedicated to your success.Receive personalized guidance and feedback.</p>
+                    </div>
+                    </div>
+                    <!-- <div class="cloned-content"></div> -->
+                    <!-- <div class="button-show">
                         <button type="button">
                             {{ trans('home.section_article1_xem_them') }}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                             </svg>
                         </button>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
